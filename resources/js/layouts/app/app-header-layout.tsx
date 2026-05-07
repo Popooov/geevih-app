4629b1a4e2aa2975902af6cc5e919111bd123f12
Oleksandr <31711254+Popooov@users.noreply.github.com>
2026-05-07 17:44:30 +0100

    fix: add Footer component to AppHeaderLayout for improved layout

--- resources/js/layouts/app/app-header-layout.tsx
+++ resources/js/layouts/app/app-header-layout.tsx
@@ -3,12 +3,14 @@ import { AppHeader } from '@/components/app-header';
 import { AppShell } from '@/components/app-shell';
 import { type BreadcrumbItem } from '@/types';
 import type { PropsWithChildren } from 'react';
+import Footer from '@/components/footer';
 
 export default function AppHeaderLayout({ children }: PropsWithChildren<{ breadcrumbs?: BreadcrumbItem[] }>) {
     return (
         <AppShell>
             <AppHeader />
             <AppContent>{children}</AppContent>
+            <Footer />
         </AppShell>
     );
 }
