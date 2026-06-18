3f4257e45400fa27657aa8cc7d287a5ad0231389
Oleksandr <31711254+Popooov@users.noreply.github.com>
2026-06-18 17:08:55 +0100

    fix: add Google site verification meta tag for SEO

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -42,6 +42,7 @@
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+        <meta name="google-site-verification" content="wNNvcMwK8QdBwj0DCg7hayZ6dlpDftiLjigfznPidBU" />
 
         @routes
         @viteReactRefresh
